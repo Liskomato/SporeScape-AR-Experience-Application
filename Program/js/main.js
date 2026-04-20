41662481dee556e71e15e88d39488377c18ff375
Tenderi4 <71285540+Tenderi4@users.noreply.github.com>
2026-04-20 22:49:54 +0300

    Fixed issue with YT player
    YT player was not playing because I forgot to initialize it. I've also set the model scale to 0.5 as that looked the best for the models' size. The textures are still borked though, but I don't have time to fix them.

--- Program/js/main.js
+++ Program/js/main.js
@@ -25,24 +25,27 @@ const createYtPlayer = () => {
 };
 
 async function startAR() {
+
+  // Initialize YouTube player
+  const player = await createYtPlayer();
     
   // Initialize MindAR and Three.js
   const mindARThreeJs = new window.MINDAR.IMAGE.MindARThree({
     container: document.body,
     imageTargetSrc: "../../assets/targets/creatures_and_adventure.mind",
-    maxTrack: 8
+    maxTrack: 9
   });
 
   const { cssRenderer, renderer, cssScene, scene, camera } = mindARThreeJs;
   const light = new THREE.HemisphereLight(0xffffff, 0xcccccc, 1);
   scene.add(light);
- 
+  
   // Add CSS object for video player
   const divVideo = new CSS3DObject(document.querySelector("#ar-example"));
 
   // Loading collada models of Spore creatures, exported natively from the game.
 
-  const modelCyborcus = await loadCollada("../../assets/models/Cyborcus.dae");
+  
   const modelHato = await loadCollada("../../assets/models/Hato.dae");
   const modelHivedegger = await loadCollada("../../assets/models/Hivedegger.dae");
   const modelSaradostrich = await loadCollada("../../assets/models/Saradostrich.dae");
@@ -50,16 +53,18 @@ async function startAR() {
   const modelZamolate = await loadCollada("../../assets/models/Zamolate.dae");
   const modelArgutix = await loadCollada("../../assets/models/Argutix.dae");
   const modelBleu = await loadCollada("../../assets/models/Bleu.dae");
+  const modelCyborcus = await loadCollada("../../assets/models/Cyborcus.dae");
 
   //Setting scale
-//   modelCyborcus.scene.scale.set(0.1,0.1,0.1);
-//   modelHato.scene.scale.set(0.1,0.1,0.1);
-//   modelHivedegger.scene.scale.set(0.1,0.1,0.1);
-//   modelSaradostrich.scene.scale.set(0.1,0.1,0.1);
-//   modelSoumadis.scene.scale.set(0.1,0.1,0.1);
-//   modelZamolate.scene.scale.set(0.1,0.1,0.1);
-//   modelArgutix.scene.scale.set(0.1,0.1,0.1);
-//   modelBleu.scene.scale.set(0.1,0.1,0.1);
+   
+   modelHato.scene.scale.set(0.5,0.5,0.5);
+   modelHivedegger.scene.scale.set(0.5,0.5,0.5);
+   modelSaradostrich.scene.scale.set(0.5,0.5,0.5);
+   modelSoumadis.scene.scale.set(0.5,0.5,0.5);
+   modelZamolate.scene.scale.set(0.5,0.5,0.5);
+   modelArgutix.scene.scale.set(0.5,0.5,0.5);
+   modelBleu.scene.scale.set(0.5,0.5,0.5);
+   modelCyborcus.scene.scale.set(0.5,0.5,0.5);
 
   // Anchoring every creation to their PNG files. (Which can also be imported back into Spore)
 
@@ -82,14 +87,6 @@ async function startAR() {
   const anchorVideo = mindARThreeJs.addCSSAnchor(5);
   anchorVideo.group.add(divVideo);
 
-  anchorVideo.onTargetFound = () => {
-    player.playVideo();
-  };
-
-  anchorVideo.onTargetLost = () => {
-    player.pauseVideo();
-  };
-
   // Resuming with anchoring creations.
   const anchorArgutix = mindARThreeJs.addAnchor(6);
   anchorArgutix.group.add(modelArgutix.scene);
@@ -100,13 +97,23 @@ async function startAR() {
   const anchorCyborcus = mindARThreeJs.addAnchor(8);
   anchorCyborcus.group.add(modelCyborcus.scene);
 
+
+
+  anchorVideo.onTargetFound = () => {
+    player.playVideo();
+  };
+
+  anchorVideo.onTargetLost = () => {
+    player.pauseVideo();
+  };
+
   // Starting application
 
   await mindARThreeJs.start();
   renderer.setAnimationLoop(render);
   function render() {
-    renderer.render(scene, camera);
     cssRenderer.render(cssScene, camera);
+    renderer.render(scene, camera);
   }
 }
 // Begin program
