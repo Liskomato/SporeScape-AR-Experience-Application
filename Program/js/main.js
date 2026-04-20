879dcd92915a216e0fb3df3dbc33868e882f0e4f
Tenderi4 <71285540+Tenderi4@users.noreply.github.com>
2026-04-20 17:05:25 +0300

    Added support for YouTube video player
    The video linked to this is an old playthrough of `SporeScape - Galaksi herää` from 2017.

--- Program/js/main.js
+++ Program/js/main.js
@@ -1,18 +1,44 @@
 import {loadCollada} from "../../libs/loader.js";
+import { CSS3DObject } from "../../libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js";
 const THREE = window.MINDAR.IMAGE.THREE;
+
+// YouTube video for SporeScape - Galaksi herää (Finnish version of The Galaxy Awakens)
+const createYtPlayer = () => {
+  return new Promise((resolve, reject) => {
+    const tag = document.createElement("script");
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.querySelectorAll("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    const onYouTubeIframeAPIReady = () => {
+      const player = new YT.Player("player", {
+        videoId: "G-L9AKXKnCM",
+        events: {
+          onReady: () => {
+            resolve(player);
+          }
+        }
+      });
+    };
+    window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+  });
+};
 
 async function startAR() {
     
   // Initialize MindAR and Three.js
   const mindARThreeJs = new window.MINDAR.IMAGE.MindARThree({
     container: document.body,
-    imageTargetSrc: "../../assets/targets/targets.mind",
+    imageTargetSrc: "../../assets/targets/creatures_and_adventure.mind",
     maxTrack: 8
   });
 
-  const { renderer, scene, camera } = mindARThreeJs;
+  const { cssRenderer, renderer, cssScene, scene, camera } = mindARThreeJs;
   const light = new THREE.HemisphereLight(0xffffff, 0xcccccc, 1);
   scene.add(light);
+ 
+  // Add CSS object for video player
+  const divVideo = new CSS3DObject(document.querySelector("#ar-example"));
 
   // Loading collada models of Spore creatures, exported natively from the game.
 
@@ -35,31 +61,44 @@ async function startAR() {
   modelArgutix.scene.scale.set(0.1,0.1,0.1);
   modelBleu.scene.scale.set(0.1,0.1,0.1);
 
-  // Anchoring every creature to their PNG files. (Which can also be imported back into Spore)
+  // Anchoring every creation to their PNG files. (Which can also be imported back into Spore)
 
-  const anchorCyborcus = mindARThreeJs.addAnchor(0);
-  anchorCyborcus.group.add(modelCyborcus.scene);
-
-  const anchorHato = mindARThreeJs.addAnchor(1);
+  const anchorHato = mindARThreeJs.addAnchor(0);
   anchorHato.group.add(modelHato.scene);
 
-  const anchorHivedegger = mindARThreeJs.addAnchor(2);
+  const anchorHivedegger = mindARThreeJs.addAnchor(1);
   anchorHivedegger.group.add(modelHivedegger.scene);
 
-  const anchorSaradostrich = mindARThreeJs.addAnchor(3);
+  const anchorSaradostrich = mindARThreeJs.addAnchor(2);
   anchorSaradostrich.group.add(modelSaradostrich.scene);
 
-  const anchorSoumadis = mindARThreeJs.addAnchor(4);
+  const anchorSoumadis = mindARThreeJs.addAnchor(3);
   anchorSoumadis.group.add(modelSoumadis.scene);
 
-  const anchorZamolate = mindARThreeJs.addAnchor(5);
+  const anchorZamolate = mindARThreeJs.addAnchor(4);
   anchorZamolate.group.add(modelZamolate.scene);
 
+  // Add video anchor
+  const anchorVideo = mindARThreeJs.addCSSAnchor(5);
+  anchorVideo.group.add(divVideo);
+
+  anchorVideo.onTargetFound = () => {
+    player.playVideo();
+  };
+
+  anchorVideo.onTargetLost = () => {
+    player.pauseVideo();
+  };
+
+  // Resuming with anchoring creations.
   const anchorArgutix = mindARThreeJs.addAnchor(6);
   anchorArgutix.group.add(modelArgutix.scene);
 
   const anchorBleu = mindARThreeJs.addAnchor(7);
   anchorBleu.group.add(modelBleu.scene);
+
+  const anchorCyborcus = mindARThreeJs.addAnchor(8);
+  anchorCyborcus.group.add(modelCyborcus.scene);
 
   // Starting application
 
@@ -67,6 +106,7 @@ async function startAR() {
   renderer.setAnimationLoop(render);
   function render() {
     renderer.render(scene, camera);
+    cssRenderer.render(cssScene, camera);
   }
 }
 // Begin program
